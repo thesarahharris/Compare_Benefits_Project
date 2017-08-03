c5334b486414651bc3670d6c36a886abb41d95e8
thesarahharris <thesarahharris@gmail.com>
2017-08-02 19:50:27 -0500

    Update: working with ranges

--- Compare_Benefits_Project/Form1.cs
+++ Compare_Benefits_Project/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
 
 namespace Compare_Benefits_Project
 {
@@ -16,5 +17,58 @@ namespace Compare_Benefits_Project
         {
             InitializeComponent();
         }
+
+        string fileExcel;
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Filter = "Excel Files|*.xls;*.xlsx;*.csv";
+            ofd.Title = "Open File Excel";
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                fileExcel = ofd.FileName;
+                textBox1.Text = ofd.SafeFileName;
+            }
+            else return;
+
+            Excel.Application xlApp;
+            Excel.Workbook xlWorkBook;
+            Excel.Worksheet xlWorkSheet;
+            object misValue = System.Reflection.Missing.Value;
+
+            xlApp = new Excel.Application();
+            //open workbook
+            xlWorkBook = xlApp.Workbooks.Open(fileExcel, 0, true, 5, "", "", true, Excel.XlPlatform.xlWindows, "\t", false, false, 0, true, 1, 0);
+            xlWorkSheet = (Excel.Worksheet)xlWorkBook.Worksheets.get_Item(1);
+            xlApp.Visible = true;
+            //MessageBox.Show(xlWorkSheet.get_Range("A1", "A1").Value2.ToSring());
+
+            //xlWorkBook.Close(true, misValue, misValue);
+            //xlApp.Quit();
+
+            //releaseObject(xlWorkSheet);
+            //releaseObject(xlWorkBook);
+            //releaseObject(xlApp);
+
+        }
+
+        //private void releaseObject(object obj)
+        //{
+        //    try
+        //    {
+        //        System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+        //        obj = null;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        obj = null;
+        //        MessageBox.Show("Unable to release the Object " + ex.ToString());
+        //    }
+        //    finally
+        //    {
+        //        GC.Collect();
+        //    }
+        //}
     }
 }
